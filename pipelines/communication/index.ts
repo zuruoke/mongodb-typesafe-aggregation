fa5e24d562b571057c7ba5adc4331366e9e0f891
zuruoke <zuruokeokafor2016@gmail.com>
2025-06-27 22:34:18 +0100

    chore: remove redundant dependencies

--- pipelines/communication/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './get-conversation-activity.pipeline';
-export * from './get-user-conversations-and-messages.pipeline';
-export * from './get-messages-by-conversation-id.pipeline';
--- pipelines/utilities/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './add-pagination-details.pipeline';
